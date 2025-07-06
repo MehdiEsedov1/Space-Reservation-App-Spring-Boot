37adbe00ea72f929c52bb662cb845dd423ccbd2d
MehdiEsedov1 <mehdiesedof@gmail.com>
2025-07-06 23:13:21 +0400

    Open project

---
+++ src/main/java/com/example/spacereservationappspringboot/SpaceReservationAppSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.spacereservationappspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceReservationAppSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpaceReservationAppSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spacereservationappspringboot/SpaceReservationAppSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spacereservationappspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceReservationAppSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
