ebfa0808d98df679371a4d6027d5611f37010501
Mehdi Asadov <mehdiesedof@gmail.com>
2025-07-19 14:44:15 +0400

    Add Factory Method Pattern

--- src/main/java/com/example/spacereservationappspringboot/mapper/WorkspaceMapper.java
+++ src/main/java/com/example/spacereservationappspringboot/mapper/WorkspaceMapper.java
@@ -1,13 +1,11 @@
 package com.example.spacereservationappspringboot.mapper;
 
-import com.example.spacereservationappspringboot.dto.request.WorkspaceRequestDTO;
 import com.example.spacereservationappspringboot.dto.response.WorkspaceResponseDTO;
 import com.example.spacereservationappspringboot.entity.Workspace;
 import org.springframework.stereotype.Component;
 
 @Component
 public class WorkspaceMapper {
-
     public WorkspaceResponseDTO toDTO(Workspace workspace) {
         return WorkspaceResponseDTO.builder()
                 .id(workspace.getId())
@@ -15,9 +13,5 @@ public class WorkspaceMapper {
                 .type(workspace.getType())
                 .price(workspace.getPrice())
                 .build();
-    }
-
-    public Workspace toEntity(WorkspaceRequestDTO dto) {
-        return new Workspace(dto.getName(), dto.getType(), dto.getPrice());
     }
 }
